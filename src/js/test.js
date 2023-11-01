936d13656eba06b4f4469a8908777cfacf33aa07
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-11-01 23:34:39 +0100

    ddd

---
+++ src/js/test.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const getPokemon = async value => {
+    try {
+        const respons = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}`);
+        console.log(respons)
+        return respons;
+    }
+    catch (error) {
+        //console.log(error)
+    }
+}
+
+getPokemon(ditto)
